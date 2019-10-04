220ca9e862865e11a4e6c348c3e1a5d54a60e72c
Jiri Kuncar <jiri.kuncar@gmail.com>
2019-10-04 20:07:02 +0200

    feat: context namespaces

--- context.py
+++ context.py
@@ -7,13 +7,49 @@ class AtLookup(type):
             return hasattr(self, f'_ld_{name[1:]}')
         return super().__contains__(name)
 
+    def __getitem__(self, name):
+        if name.startswith('@'):
+            return getattr(self, f'_ld_{name[1:]}')
+        return super().__getitem__(name)
+        # return NS(self._ld_type + name)
 
-class Node(metaclass=AtLookup):
-    """Base node for linked data graph."""
+    def __truediv__(self, name):
+        return NS(f'{self._ld_type}{name}', final=True)
 
-    def __init_subclass__(cls, context=None, type=None, **kwargs):
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self._ld_type == other
+        raise NotImplementedError
+
+
+class Base:
+    """Base class for linked data graph."""
+
+    def __init_subclass__(cls, context=None, type=None, final=True, **kwargs):
         """Initialize context and type."""
         super().__init_subclass__(**kwargs)
         cls._ld_context = context or {}
+
+        # for non-final annotations create new final NS / key
+        cls._ld_context.update({
+            key: value if value._ld_final else value / key
+            for key, value in getattr(cls, '__annotations__', {}).items()
+            if issubclass(value, Base)
+        })
+
         cls._ld_type = type
+        cls._ld_final = final
         return cls
+
+
+class Node(Base, metaclass=AtLookup):
+    pass
+
+
+def NS(namespace, final=False):
+    class Namespace(Node, type=namespace, final=final):
+
+        def __init_subclass__(cls, **kwargs):
+            super().__init_subclass__(type=f'{namespace}{cls.__name__}', **kwargs)
+
+    return Namespace
--- test_context.py
+++ test_context.py
@@ -7,3 +7,36 @@ def test_empty_context():
         pass
 
     assert '@context' in Empty
+
+
+def test_type():
+
+    PROV = context.NS('http://www.w3.org/ns/prov#')
+
+    class Activity(PROV):
+        pass
+
+    assert Activity['@type'] == PROV['@type'] + 'Activity'
+
+
+def test_field():
+
+    FOAF = context.NS('http://xmlns.com/foaf/0.1/')
+
+    class Field(context.Node):
+        field: FOAF / 'name' = 'Fieldname'
+
+    assert Field['@context']['field'] == 'http://xmlns.com/foaf/0.1/name'
+    assert Field.field == 'Fieldname'
+
+
+def test_field_with_pure_namespace_type():
+
+    FOAF = context.NS('http://xmlns.com/foaf/0.1/')
+
+    class Field(context.Node):
+        name: FOAF = 'Fieldname'
+
+    assert Field['@context']['name'] == 'http://xmlns.com/foaf/0.1/name'
+    assert Field.name == 'Fieldname'
+
